d93334a34e65a31cf3dc15422510fb1c88fe5dd6
lacry <lacryk@gmail.com>
2024-10-23 20:48:41 +0300

    migrate to cbv, create blog

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from . import views
+from .views import ProductDeleteView, ProductUpdateView, ProductCreateView, ProductDetailView, ProductListView, ContactsView
 
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('contacts/', views.contacts, name='contacts'),
-    path('catalog/<int:pk>/', views.product_detail, name='product_detail'),
+    path('', ProductListView.as_view(), name='home'),
+    path('product/new/', ProductCreateView.as_view(), name='product_create'),
+    path('products/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('product/update/<int:pk>/', ProductUpdateView.as_view(), name='product_update'),
+    path('product/delete/<int:pk>/', ProductDeleteView.as_view(), name='product_delete'),
+    path('contacts/', ContactsView.as_view(), name='contacts'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,29 +1,63 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic import ListView, DetailView, DeleteView, FormView
 from catalog.models import Product
+from django.contrib import messages
+from django.core.mail import send_mail
+from .templates.forms.forms import ContactForm
 
 
-def product_detail(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    context = {'product': product}
-    return render(request, 'product_detail.html', context)
+class ProductListView(ListView):
+    model = Product
+    template_name = 'catalog/home.html'
+    context_object_name = 'products'
 
 
-def home(request):
-    products = Product.objects.all()
-    context = {'products': products}
-    return render(request, 'home.html', context)
+class ProductCreateView(CreateView):
+    model = Product
+    fields = ['name', 'description', 'image', 'category', 'price']
+    template_name = 'catalog/product_form.html'
+    success_url = reverse_lazy('catalog:products_list')
 
 
-def about(request):
-    return render(request, 'catalog/about.html')
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'catalog/product_detail.html'
+    context_object_name = 'product'
 
 
-def contacts(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        message = request.POST.get('message')
+class ProductUpdateView(UpdateView):
+    model = Product
+    fields = ['name', 'description', 'image', 'category', 'price']
+    template_name = 'catalog/product_form.html'
+    success_url = reverse_lazy('catalog:products_list')
 
-        return HttpResponse(f'Спасибо, {name}! Ваше сообщение "{message}" получено.')
-    return render(request, 'contacts.html')
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = 'catalog/product_confirm_delete.html'
+    success_url = reverse_lazy('catalog:products_list')
+
+
+class ContactsView(FormView):
+    template_name = 'catalog/contacts.html'
+    form_class = ContactForm
+    success_url = reverse_lazy('catalog:contacts')  # Здесь укажи URL для перенаправления после успешной отправки
+
+    def form_valid(self, form):
+        name = form.cleaned_data['name']
+        message = form.cleaned_data['message']
+
+        # send_mail(
+        #     subject=f'Новое сообщение от {name}',
+        #     message=message,
+        #     from_email='your_email@gmail.com',
+        #     recipient_list=['recipient_email@example.com'],
+        # )
+
+        messages.success(self.request, f'Спасибо, {name}! Ваше сообщение "{message}" получено.')  # Добавляем сообщение об успехе
+        return super().form_valid(form)  # Вызовем метод родителя для перенаправления на success_url
+
+    def form_invalid(self, form):
+        # Если форма недействительна, просто отобразим шаблон с ошибками
+        return super().form_invalid(form)
